6d3bcb8c17d6d6b5d8f024c8dd7694e4ef82dd85
jrainey12 <jrainey12@qub.ac.uk>
2022-02-07 18:15:40 +0000

    Updated sample_gender to work with single,GEI and keyframe data. Also changed sampling to downsample larger class to get a balanced dataset and set the number of samples to size of smaller class. This way the data can be augmented before classification to increase dataset size.

--- utils/sample_gender.py
+++ utils/sample_gender.py
@@ -3,81 +3,117 @@ from random import seed,choice
 import random
 import argparse
 import os
-from pathlib import Path
+from sklearn.utils import resample
 
-
-def main(base_dir, no_samples,sample_type, out_dir):
+def main(base_dir, sample_type, out_dir, model_var):
     """
     Sample a dataset and save the samples in a new folder.
     Args:
-        base_dir(str): Directory containing data to be sampled.
-        no_samples(int): Number of samples to save.
-        sample_type(train,test,val): Type of data being sampled.
-        out_dir(str): Directory to save sampled data.
+        base_dir(str) - Directory containing data to be sampled.
+        sample_type(train,test,val) - Type of data being sampled.
+        out_dir(str) - Directory to save sampled data.
+        model_var(single,GEI,keyframe) - Model variation being trained. 
     """
 
     #Set up male and female input paths
-    b = Path(base_dir)
-    m_in_dir = b / "male"
-    f_in_dir = b / "female"
+    m_in_dir = os.path.join(base_dir,"male")
+    f_in_dir = os.path.join(base_dir, "female")
+
+    print (m_in_dir)
+    print (f_in_dir)
 
     #Set up male and female output paths
-    o = Path(out_dir)
-    m_out_dir = o / sample_type / "male"
-    f_out_dir = o / sample_type / "female"
+    m_out_dir = os.path.join(out_dir, sample_type, "male")
+    f_out_dir = os.path.join(out_dir, sample_type, "female")
+
+    #get data
+    male_data = get_dataset(m_in_dir,model_var)
+    female_data = get_dataset(f_in_dir,model_var)
+
+    #Find min size of dataset and set as no_samples, effectively downsampling larger of the two.
+    no_samples = min(len(male_data),len(female_data))
+    print("Number of samples: ", no_samples)
 
     #Sample for male and female
-    sample(str(m_in_dir),no_samples,str(m_out_dir))
-    sample(str(f_in_dir),no_samples,str(f_out_dir))
+    sample(male_data,no_samples,m_out_dir)
+    sample(female_data,no_samples,f_out_dir)
+
+def get_dataset(in_dir,model_var):
+    """
+    Get all datapoints at the lowest point of the input directory tree.
+    Args:
+        in_dir(str)- Input directory tree.
+        model_var(str) - Model variation: single,GEI or keyframe.
+    Return:
+        sils/GEIs/keyframes(list) - returns a list of the datapoints from the selected variation.
+    """
+    lowest_dirs = []
+
+    variations = ["nm-01","nm-02","nm-03","nm-04","nm-05","nm-06"]
+
+    #Single Frame
+    if model_var == "single":
+        sils = []
+        #get all files in dataset to be sampled 
+        for root,dirs,files in os.walk(in_dir):
+            if files and not dirs:
+                if any(var in root for var in variations):
+                    for f in files:
+                        sils.append(root + "/"+ f)
+        
+        return sils
+        
+    #GEI
+    elif model_var == "GEI":
+        GEIs = []
+        #get all files in dataset to be sampled 
+        for root,dirs,files in os.walk(in_dir):
+            if files and not dirs:
+                if any(var in root for var in variations):
+                    for f in files:
+                        GEIs.append(root + "/"+ f)
+
+        return GEIs
+
+    #Keyframes
+    elif model_var == "keyframe":
+        keyframes = []
+        #get all files in dataset to be sampled 
+        for root,dirs,files in os.walk(in_dir):
+            #print(root)
+            if files and not dirs:
+                if any(var in root for var in variations):
+                    for f in files:
+                        #print(f)
+                        if f == "stack_mean_keyframe.png":
+                            keyframes.append(root + "/"+ f)
+
+        return keyframes
 
 
-def sample(in_dir, no_samples, out_dir):
+def sample(data, no_samples, out_dir):
     """
     Randomly select a sample from the input data.
     Args:
-        in_dir(str): Directory containing data to be sampled.
-        no_samples(str): Number of samples to save.
-        out_dir(str): Directory to save samples.
+        in_dir(str) - Directory containing data to be sampled.
+        no_samples(int) - Number of samples to save.
+        out_dir(str) - Directory to save samples.
     """
-
-    #TODO: Add a method of selecting/ignoring some varitions or angles.
-    # e.g only selecting nm-01 to nm-04 and ignoring nm-05 and nm-06.
-    # This would prevent having to move files.
-    # Possibly after getting sils I could remove any containing selected strings.
-
-    #TODO: Fix Path based approach, currently uses both Path and os, should use 
-    # either one or the other.
 
     #Set seed to sample size to ensure data is consistent when
     # same size dataset is sampled on a set of data.
     random.seed(no_samples)
-
-    out = Path(out_dir)
-    if not out.exists():
-        os.makedirs(out)
     
-    indir = Path(in_dir)
-
-    subjects = list(indir.glob("*"))
-   
-    lowest_dirs = []
-    sils = []
-    #get all files in dataset to be sampled 
-    for root,dirs,files in os.walk(in_dir):
-        if files and not dirs:
-            for f in files:
-                sils.append(root + "/"+ f)
-
-    print(len(sils))
-
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    
     #get samples
-    samples = random.sample(sils, no_samples)
-    #print(len(samples))
+    samples = random.sample(data, no_samples)
 
     #Copy samples to out directory
     for i,s in enumerate(samples):
         #print (i, s)
-        outname = out / str("%03d.png" % (i + 1))
+        outname = os.path.join(out_dir,str("%03d.png" % (i + 1)))
         copyfile(s, outname)
 
 
@@ -88,12 +124,7 @@ if __name__=='__main__':
     parser.add_argument(
         'base_dir',
         help="Directory that contains the data to be sampled.")   
-    
-    parser.add_argument(
-        'no_samples',
-        type=int,
-        help="No of samples to save.")   
-         
+        
     parser.add_argument(
         'sample_type',
         help="Sample type: train, val or test.")   
@@ -102,6 +133,12 @@ if __name__=='__main__':
         'out_dir',
         help="Directory to save samples.")
 
+    parser.add_argument(
+            'model_var',
+            choices=("single","GEI","keyframe"),
+            default="single",
+            help="Model variation being trained. single, GEI, or keyframe.")
+
     args = parser.parse_args()
 
-    main(args.base_dir, args.no_samples, args.sample_type, args.out_dir) 
+    main(args.base_dir, args.sample_type, args.out_dir, args.model_var) 
